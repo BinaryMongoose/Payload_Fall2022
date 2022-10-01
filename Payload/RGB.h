8324f8cfd5a3dec853c5f5174930fc798c0c6da2
BinaryMongoose <96893291+BinaryMongoose@users.noreply.github.com>
2022-10-01 14:23:38 -0700

    Created Payload Master

--- Payload/RGB.h
+++
@@ -1,9 +0,0 @@
-#ifndef RGB
-#define RGB
-
-#include <Arduino.h>
-
-void Flash(int RGB_PINS[], int RGB_COLOR[]);
-void RGB_Light(int RGB_PINS[], int RGB_COLOR[]);
-
-#endif
--- Payload/colors.h
+++
@@ -1,6 +0,0 @@
-// int CLEAR[3] = {0, 0, 0}; // Clears the LED. 
-
-int WHITE[3] = {255, 255, 255};
-int RED[3]   = {255, 0, 0};
-int GREEN[3] = {0, 255, 0};
-int BLUE[3]  = {0, 0, 255};
