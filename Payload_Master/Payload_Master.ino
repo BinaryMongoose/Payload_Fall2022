0444e1db6ae0383fa58a496d0c5e2d024d14e7de
BinaryMongoose <96893291+BinaryMongoose@users.noreply.github.com>
2022-10-28 14:42:02 -0700

    Coded file creation and write.

--- Payload_Master/Payload_Master.ino
+++ Payload_Master/Payload_Master.ino
@@ -3,7 +3,7 @@
 #include <SPI.h>
 #include <SD.h>
 
-File data;
+File currentFile;
 
 int const RGB_PINS[3] = { 2, 3, 4 };
 
@@ -28,11 +28,9 @@ void setup() {
 
   Serial.println("initialization done. (I did what I was supposed to!)");
 
-  data = SD.open("data.txt", FILE_WRITE);
+  currentFile = SD.open("data.txt", FILE_WRITE);
 
-    // if the file opened okay, write to it:
-
-  if (data) {
+  if (currentFile) {
     Serial.println("Data is open.");
   } else {
     // if the file didn't open, print an error:
@@ -46,22 +44,26 @@ void setup() {
   }
 }
 
+int currentFileNum = 1;
 void loop() {
-  
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= interval) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
+  if (SD.begin(4)) {
+    if (currentMillis - previousMillis >= interval) {
+      // save the last time you blinked the LED
+      previousMillis = currentMillis;
 
-    data.close();
-    Serial.println("data file is colsed.");
+      currentFile.close();
+      Serial.println("data file is closed.");
 
-    data = SD.open("data.txt", FILE_WRITE);
-    Serial.println("data is open.");
+      currentFile = SD.open("data_" + String(currentFileNum) + ".txt", FILE_WRITE);
+      Serial.println("data is open.");
 
-    data.println(currentMillis/1000);
+      currentFile.println(currentMillis/1000);
+      Serial.println(currentMillis/1000);
+      currentFileNum += 1;
+    }
+  } else {
+    Serial.println("Put the SD card in!");
   }
-
-
 }
